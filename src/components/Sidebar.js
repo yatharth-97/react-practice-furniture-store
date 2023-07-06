fff6cdfc530f58971e5c47d03f5efa3f9d8eebd5
yatharth-97 <yatharthshahrawat97@gmail.com>
2023-07-07 01:09:01 +0530

    changes in Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,6 @@ import { useProductsContext } from '../context/products_context';
 import { FaTimes } from 'react-icons/fa';
 import { links } from '../utils/constants';
 import styled from 'styled-components';
-import CartButtons from './CartButtons';
 import { useUserContext } from '../context/user_context';
 
 const Sidebar = () => {
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
